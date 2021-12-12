49c748df76fa4393f07b99d2981c3865e875ba97
Luiz Gustavo Ghisi <ghisiluizgustavo@gmail.com>
2021-12-12 11:50:36 -0300

    (feat): finished migrates

---
+++ src/main/resources/db/migration/V2__inserts_of_all_articles_spaceflightnewsapi.sql
@@ -0,0 +1,139 @@
+INSERT INTO article VALUES (
+                               13264,
+                               false,
+                               'Parsons to develop prototype ground operations center for DARPA’s Blackjack satellites',
+                               'https://spacenews.com/parsons-to-develop-prototype-ground-operations-center-for-darpas-blackjack-satellites/',
+                               'https://spacenews.com/wp-content/uploads/2020/03/DARPA-470x470.png',
+                               'SpaceNews',
+                               'The Defense Advanced Research Projects Agency awarded Parsons a $10.8 million contract to prototype a ground
+                                        operations center for the agency’s Blackjack constellation.',
+                               '2021-12-11T20:30:12.000Z',
+                               '2021-12-11T20:30:12.528Z'
+                           );
+
+INSERT INTO article VALUES (
+                               13261,
+                               false,
+                               'Michael Strahan, Alan Shepard’s daughter and four others rocket into space',
+                               'https://spaceflightnow.com/2021/12/11/michael-strahan-alan-shepards-daughter-and-four-others-rocket-into-space/',
+                               'https://spaceflightnow.com/wp-content/uploads/2021/12/ns19quick1.jpg',
+                               'Spaceflight Now"',
+                               'Sixty years after her father became the first American in space, Laura Shepard Churchley, daughter of Mercury
+                                        astronaut Alan Shepard, rocketed out of the lower atmosphere Saturday, joining ABC TV host Michael Strahan
+                                        and four others for a thrilling 10-minute climb to space and back.',
+                               '2021-12-11T18:50:07.000Z',
+                               '2021-12-11T20:00:24.910Z'
+                           );
+
+INSERT INTO article VALUES (
+                               13259,
+                               false,
+                               'Blue Origin launches first six-person New Shepard suborbital flight',
+                               'https://spacenews.com/blue-origin-launches-first-six-person-new-shepard-suborbital-flight/',
+                               'https://spacenews.com/wp-content/uploads/2021/12/ns19-descending.jpg',
+                               'SpaceNews',
+                               'Blue Origin launched its third crewed New Shepard suborbital mission Dec. 11, carrying six people on a brief
+                                        flight to the edge of space.',
+                               '2021-12-11T15:40:12.000Z',
+                               '2021-12-11T15:40:12.227Z'
+                           );
+
+INSERT INTO article VALUES (
+                               13255,
+                               false,
+                               'Blue Origin launches NS-19 with full passenger complement',
+                               'https://www.nasaspaceflight.com/2021/12/ns-19/',
+                               'https://www.nasaspaceflight.com/wp-content/uploads/2021/07/E6vjvCuX0AUN0j5.jpg',
+                               'NASA Spaceflight',
+                               'Blue Origin launched a crew of six people on board a New Shepard rocket on Saturday, December 11 from the
+                                        company’s West Texas facility at Corn Ranch. The liftoff took place at 9:01 am CST at the launch site, or 15:01 UTC.',
+                               '2021-12-11T12:04:12.000Z',
+                               '2021-12-11T20:00:37.947Z'
+                           );
+
+INSERT INTO article VALUES (
+                               13252,
+                               false,
+                               'FAA to end commercial astronaut wings program',
+                               'https://spacenews.com/faa-to-end-commercial-astronaut-wings-program/',
+                               'https://spacenews.com/wp-content/uploads/2019/02/cj-astrowings.jpg',
+                               'SpaceNews',
+                               'The Federal Aviation Administration will stop awarding commercial astronaut wings at the end of this year,
+                                        five months after it revised the criteria for receiving the wings.',
+                               '2021-12-10T23:20:13.000Z',
+                               '2021-12-10T23:20:13.422Z'
+                           );
+
+INSERT INTO article VALUES (
+                               13251,
+                               false,
+                               'MDA discusses status, mission objectives of Canada’s LEAP lunar rover',
+                               'https://www.nasaspaceflight.com/2021/12/leap-update-dec-2021/',
+                               'https://www.nasaspaceflight.com/wp-content/uploads/2020/12/lune-section-info-role-du-canada.jpg',
+                               'NASA Spaceflight"',
+                               'Humanity’s next steps in space exploration are focused on exploring like we never have before: returning to
+                                       the Moon to establish a permanent, sustainable base as we move on to stepping foot on Mars — all of which the
+                                       Canadian Space Agency (CSA) is actively participating in',
+                               '2021-12-10T21:16:28.000Z',
+                               '2021-12-10T21:20:12.741Z'
+                           );
+
+INSERT INTO article VALUES (
+                               13256,
+                               false,
+                               'Blue Origin poised for first launch with crew of six',
+                               'https://spaceflightnow.com/2021/12/10/alan-shepards-daughter-micheal-strahan-among-six-launching-to-space-saturday/',
+                               'https://spaceflightnow.com/wp-content/uploads/2021/12/crew1.jpg',
+                               'Spaceflight Now',
+                               'Sixty years after NASA astronaut Alan Shepard became the first American in space, his oldest daughter plans to
+                                        blast off Saturday on a sub-orbital flight of her own, joining former NFL star and TV host Michael Strahan, two entrepreneurs
+                                        and a father-and-son duo aboard a Blue Origin New Shepard spacecraft.',
+                               '2021-12-10T19:00:08.000Z',
+                               '2021-12-11T20:00:58.044Z'
+                           );
+
+INSERT INTO article VALUES (
+                               13249,
+                               false,
+                               'Saving the shuttle simulator—“It was an artifact that needed to be preserved',
+                               'https://arstechnica.com/science/2021/12/saving-the-shuttle-simulator-it-was-an-artifact-that-needed-to-be-preserved/',
+                               'https://cdn.arstechnica.net/wp-content/uploads/2021/12/2021-1110E-Cockpit-Low-Angle.jpg',
+                               'Arstechnica',
+                               'We hope to have it in place at the museum in April.',
+                               '2021-12-10T14:57:36.000Z',
+                               '2021-12-10T15:10:16.997Z'
+                           );
+
+INSERT INTO article VALUES (
+                               13248,
+                               false,
+                               'Virgin Orbit adds Spire satellite to next launch',
+                               'https://spacenews.com/virgin-orbit-adds-spire-satellite-to-next-launch/',
+                               'https://spacenews.com/wp-content/uploads/2020/05/launcherone-inflight.jpg',
+                               'SpaceNews',
+                               'Virgin Orbit will fly a Spire cubesat in addition to several other payloads on its next LauncherOne launch,
+                                       scheduled for no earlier than Dec. 22.',
+                               '2021-12-10T12:20:11.000Z',
+                               '2021-12-10T12:20:11.516Z'
+                           );
+
+INSERT INTO article VALUES (
+                               13247,
+                               false,
+                               'Rocket Report: Astra to launch from Florida, NASA troubleshoots SLS issue',
+                               'https://arstechnica.com/science/2021/12/rocket-report-astra-to-launch-from-florida-nasa-troubleshoots-sls-issue/',
+                               'https://cdn.arstechnica.net/wp-content/uploads/2021/12/GwtTgveA.jpeg',
+                               'Arstechnica',
+                               '\"Construction of Starship orbital launch pad at the Cape has begun.\"',
+                               '2021-12-10T12:00:01.000Z',
+                               '2021-12-10T12:37:53.128Z'
+                           );
+
+INSERT INTO launch VALUES ('6b0311ed-1ece-4485-89f8-6832cb5e2358','Launch Library 2', 13261);
+
+INSERT INTO launch VALUES ('6b0311ed-1ece-4485-89f8-6832cb5e2358','Launch Library 2', 13255);
+
+INSERT INTO launch VALUES ('6b0311ed-1ece-4485-89f8-6832cb5e2358','Launch Library 2', 13256);
+
+INSERT INTO launch VALUES ('65896761-b6ca-4df3-9699-e077a360c52a','Launch Library 2', 13247);
+INSERT INTO launch VALUES ('2b74e95d-de9d-44e5-aefb-d1e48d9e35e6','Launch Library 2', 13247);
